c07bbaffb1205aba2dfd66f1de3e3053cf166fa8
beatrizherculano <biaher.oliveira@gmail.com>
2023-10-23 15:50:37 -0300

    Initial Commit

---
+++ ZarioTest/ZarioTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZarioTestApp.swift
+//  ZarioTest
+//
+//  Created by Beatriz Herculano on 23/10/23.
+//
+
+import SwiftUI
+
+@main
+struct ZarioTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
